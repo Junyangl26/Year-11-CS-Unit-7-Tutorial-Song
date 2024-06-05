5f075c8fb4a74a1a622ff9099a07823d1aa735be
Junyang <junyangl2026@student.cis.edu.hk>
2024-06-05 13:34:45 +0800

    ok

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    ArrayList<String> oldListener = new ArrayList<>();
+    private String title;
+    private String artist;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> listeners) {
+        int cnt = 0;
+        for (String x : listeners) {
+            String regardless = x.toLowerCase();
+            if (!oldListener.contains(regardless)) {
+                oldListener.add(regardless);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }
